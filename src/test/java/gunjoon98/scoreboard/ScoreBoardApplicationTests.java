bdb70e50eb1ac14a1a18e08d843a1eb66cd6882a
JeongGunJoon <dlans321@naver.com>
2023-05-03 13:13:50 +0900

    feat: spring initiallization

---
+++ src/test/java/gunjoon98/scoreboard/ScoreBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package gunjoon98.scoreboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScoreBoardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
